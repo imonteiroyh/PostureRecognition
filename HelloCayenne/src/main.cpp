86b869b8815ffae207db16899712cd6cfe0370ed
Davi Queiroz Albuquerque <daviqueiroz2002.dq@gmail.com>
2023-04-13 22:47:04 -0300

    Versão para apresentação das features da plataforma Cayenne

--- HelloCayenne/src/main.cpp
+++ HelloCayenne/src/main.cpp
@@ -8,18 +8,23 @@
 #define TEMPERATURE_CHANNEL 1
 #define HUMIDITY_CHANNEL 2
 #define PROXIMITY_CHANNEL 3
+#define LED_CHANNEL 4
 
 #define PIN_DHT 16
 #define DHT_TYPE DHT11
 
-#define SR04_ECHO 12
-#define SR04_TRIGGER 13
+#define SR04_ECHO 27
+#define SR04_TRIGGER 14
+#define PRESENCE_THRESHOLD 30
+
+#define PIN_LED 13
 
 DHT dht(PIN_DHT, DHT_TYPE);
 int temperature, humidity;
 
 Ultrasonic ultrasonic(SR04_TRIGGER, SR04_ECHO);
 float distance;
+bool presenceDetected;
 
 void readTemperatureAndHumidity(){
 	do{
@@ -30,10 +35,24 @@ void readTemperatureAndHumidity(){
 	Serial.printf("Temperatura: %d / Umidade: %d %%\n", temperature, humidity);
 }
 
+void readDistance(){
+	long microsec = ultrasonic.timing();
+	distance = ultrasonic.convert(microsec, Ultrasonic::CM);
+}
+
+bool detectPresence(){
+	if(distance <= PRESENCE_THRESHOLD)
+		return true;
+	else
+		return false;
+}
+
 void setup() {
 	Serial.begin(9600);
 	Cayenne.begin(username, mqtt_password, client_id, ssid, wifi_password);
 	dht.begin();
+	pinMode(PIN_LED, OUTPUT);
+	digitalWrite(PIN_LED, LOW);
 }
 
 void loop() {
@@ -42,5 +61,13 @@ void loop() {
 	Cayenne.celsiusWrite(TEMPERATURE_CHANNEL, temperature);
 	Cayenne.virtualWrite(HUMIDITY_CHANNEL, humidity);
 
+	readDistance();
+	presenceDetected = detectPresence();
+	Serial.printf("Presença: %d\n", (int)presenceDetected);
+	Cayenne.digitalSensorWrite(PROXIMITY_CHANNEL, presenceDetected);
 	sleep(2);
+}
+
+CAYENNE_IN(LED_CHANNEL){
+	digitalWrite(PIN_LED, getValue.asInt());
 }
