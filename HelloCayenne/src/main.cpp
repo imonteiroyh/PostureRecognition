8cd4c17d6252efce16307be164bdcbdade1931ee
Davi Queiroz Albuquerque <daviqueiroz2002.dq@gmail.com>
2023-04-13 21:33:20 -0300

    Enviando temperatura e umidade para a dashboard

--- HelloCayenne/src/main.cpp
+++ HelloCayenne/src/main.cpp
@@ -1,40 +1,46 @@
 #include <CayenneMQTTESP32.h>
 #include <DHT.h>
+#include <Ultrasonic.h>
+
+#include "config.h"
 
 #define CAYENNE_PRINT Serial
-
-char ssid[] = "";
-char wifi_password[] = "";
-
-char username[] = "";
-char mqtt_password[] = "";
-char client_id[] = "";
-
-#define PIN_POWER 17
-#define PIN_GND 4
+#define TEMPERATURE_CHANNEL 1
+#define HUMIDITY_CHANNEL 2
+#define PROXIMITY_CHANNEL 3
 
 #define PIN_DHT 16
 #define DHT_TYPE DHT11
 
+#define SR04_ECHO 12
+#define SR04_TRIGGER 13
+
 DHT dht(PIN_DHT, DHT_TYPE);
-int t;
+int temperature, humidity;
+
+Ultrasonic ultrasonic(SR04_TRIGGER, SR04_ECHO);
+float distance;
+
+void readTemperatureAndHumidity(){
+	do{
+		temperature = (int)dht.readTemperature();
+		humidity = (int)dht.readHumidity();
+	} while(isnan(temperature) || isnan(humidity));
+
+	Serial.printf("Temperatura: %d / Umidade: %d %%\n", temperature, humidity);
+}
 
 void setup() {
 	Serial.begin(9600);
 	Cayenne.begin(username, mqtt_password, client_id, ssid, wifi_password);
-	pinMode(PIN_POWER, OUTPUT);
-	pinMode(PIN_GND, OUTPUT);
-	digitalWrite(PIN_POWER, HIGH);
-	digitalWrite(PIN_GND, LOW);
+	dht.begin();
 }
 
 void loop() {
-	Cayenne.loop(100);
-}
+	Cayenne.loop();
+	readTemperatureAndHumidity();
+	Cayenne.celsiusWrite(TEMPERATURE_CHANNEL, temperature);
+	Cayenne.virtualWrite(HUMIDITY_CHANNEL, humidity);
 
-CAYENNE_OUT(1){
-	CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
-	t = dht.readTemperature();
-	Cayenne.celsiusWrite(1, t);
-	Serial.printf("Temperatura: %d\n", t);
+	sleep(2);
 }
