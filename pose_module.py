985c04206f045b8368b6884ba15720cedb7b3af1
imonteiroyh <imonteiroyh@gmail.com>
2023-05-11 10:29:02 -0300

    Estimação da pose de vídeos/webcam

---
+++ pose_module.py
@@ -0,0 +1,47 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+
+class PoseDetector():
+
+    def __init__(self, static_image_mode=False, model_complexity=1, smooth_landmarks=True, enable_segmentation=True, smooth_segmentation=True, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.static_image_mode = static_image_mode
+        self.model_complexity = model_complexity
+        self.smooth_landmarks = smooth_landmarks
+        self.enable_segmentation = enable_segmentation
+        self.smooth_segmentation = smooth_segmentation
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.static_image_mode, self.model_complexity, self.smooth_landmarks,
+                                     self.enable_segmentation, self.smooth_segmentation,
+                                     self.min_detection_confidence, self.min_tracking_confidence)
+
+    def get_pose(self, frame, draw=True):
+        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(frameRGB)
+
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(frame, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS,
+                                        connection_drawing_spec=self.mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2))
+
+
+        return frame
+
+
+    def get_position(self, frame):
+        landmarks = []
+
+        if self.results.pose_landmarks:
+            for id, landmark in enumerate(self.results.pose_landmarks.landmark):
+                height, width, channel = frame.shape
+                cx, cy = int(landmark.x * width), int(landmark.y * height)
+
+                landmarks.append([id, cx, cy])
+
+        return landmarks
