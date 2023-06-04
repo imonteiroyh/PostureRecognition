6e43e35039265ccc5a30e27c08c7ff1100999ec2
imonteiroyh <imonteiroyh@gmail.com>
2023-06-04 09:23:59 -0300

    Corrigindo detecção de múltiplos indivíduos

--- posture_detection.py
+++ posture_detection.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 import argparse
+import pandas as pd
 from posture_classification.pose_module import PoseEstimator
 from posture_classification.posture_module import PostureClassifier
 
@@ -19,21 +20,35 @@ if __name__ == '__main__':
     estimator = PoseEstimator()
 
     # Imagem
+    data = pd.DataFrame()
     if args.type == 'image':
 
-        for file in os.listdir('image'):
+            for folder in ['correct/', 'incorrect/']:
 
-            frame = cv2.imread('image/' + file)
+                subfolders = ['arm_up/', 'chest_sloped/', 'chest_turned/', 'legs_up/', 'neck_sloped/', 'neck_turned/'] if folder == 'incorrect/' else ['/']
+                for subfolder in subfolders:
+                    i = 0
+                    for file in os.listdir('image/' + folder + subfolder):
+                        frame = cv2.imread('image/' + folder + subfolder + file)
 
-            start = time.time()
-            frame, body_marks = estimator.process_capture(frame)
-            classifier = PostureClassifier(body_marks)
-            print(f'Tempo: {time.time() - start}')
+                        start = time.time()
+                        frame, body_marks = estimator.process_capture(frame)
+                        classifier = PostureClassifier(body_marks)
 
-            cv2.imwrite('image_processed/' + file, frame)
-            
-            cv2.imshow(file, frame)
-            cv2.waitKey(0)
+                        info = classifier.get_info()
+                        info_dataframe = pd.DataFrame.from_dict(info, orient='index').T
+                        info_dataframe['file'] = file
+                        info_dataframe['class'] = True if folder == 'incorrect/' else False
+
+                        data = pd.concat([data, info_dataframe], ignore_index=True)
+                        print(f'Tempo: {time.time() - start}')
+
+                        # cv2.imshow(file, frame)
+                        # cv2.waitKey(0)
+
+                        # cv2.imwrite('image_processed/' + file, frame)
+
+
 
     # Video
     if args.type == 'video':
@@ -100,3 +115,4 @@ if __name__ == '__main__':
             print('Não foi possível abrir a câmera')
 
 cv2.destroyAllWindows()
+data.to_csv('features_dataset.csv')
