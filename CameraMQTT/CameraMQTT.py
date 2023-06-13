d73ccaa199d80800c811653f029a934831981a8f
Davi Queiroz <daviqueiroz2002.dq@gmail.com>
2023-06-12 21:28:16 -0300

    Adicionando opção de rotacionar imagem capturada

--- CameraMQTT/CameraMQTT.py
+++ CameraMQTT/CameraMQTT.py
@@ -13,13 +13,13 @@ BROKER_PORT = 1883
 IMAGES_TOPIC = 'images'
 
 CAMERA_RESOLUTION = (640, 480)
-CAPTURE_PERIOD = 5 # in seconds
+CAPTURE_PERIOD = 10 # in seconds
 
 
-def capture(camera: PiCamera, image_stream: BytesIO, resolution: tuple, format='jpeg', exposure_mode='auto', vflip=False):
+def capture(camera: PiCamera, image_stream: BytesIO, resolution: tuple, format='jpeg', rotation=0, vflip=False):
     camera.resolution = resolution
-    camera.exposure_mode = exposure_mode
     camera.vflip = vflip
+    camera.rotation = rotation
     camera.capture(image_stream, format= format)
 
 
@@ -39,7 +39,7 @@ def main():
         try:
             image_stream = BytesIO()
 
-            capture(camera, image_stream, CAMERA_RESOLUTION, vflip=True)
+            capture(camera, image_stream, CAMERA_RESOLUTION, rotation=90)
             
             image = read_image(image_stream)
             image_stream.close()
